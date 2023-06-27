f8a7f7bc16906369070f4ca2f3f27cba2caf82e8
Pusana007 <pusana.ju.65@ubu.ac.th>
2023-06-27 15:32:23 +0700

    My Profile

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App(){
+  return(
+    <>
+      <div className='card'>
+        <Avatar />
+        <div className='data'>
+          <Intro />
+          <SkillList />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      <Skill skill='pizza' emoji='🤌' color='red' />
+      <Skill skill='burgur' emoji='👌' color='blue' />
+      <Skill skill='python' emoji='👉👈' color='pink' />
+      <Skill skill='javascript' emoji='🦾' color='green' /> 
+    </div>
+  );
+}
+
+function Skill(props){
+  return(
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro(){
+  return(
+    <div>
+      <h1>นายภูษณ จันภักดี</h1>
+      <p>
+        นักศึกษาที่จะเป็นโปรแกรมอันดับ 1 ของโลก จากสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์
+        มหาวิทยาลัยอุบลราชธานี ชืนชอบเกมออนไลน์ และอยากมีเงินเก็บเยอะๆ
+      </p>
+    </div>
+  );
+}
+
+function Avatar(){
+  return <img className='avatar' src='ass1.jpg' alt="My Avatar"></img>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +58,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
